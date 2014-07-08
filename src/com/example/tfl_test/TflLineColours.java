d8c64aba41ae2d4482451f4a518c8736fc95e3c2
Blanca Garcia Gil <bgarciagil@gmail.com>
2014-07-08 17:21:09 +0100

    Add colour for each tube line

--- src/com/example/tfl_test/TflLineColours.java
+++ src/com/example/tfl_test/TflLineColours.java
@@ -1,41 +1,45 @@
 package com.example.tfl_test;
 
+import android.graphics.Color;
+
 public enum TflLineColours {
 
-    BAKERLOO (1, "Bakerloo", 0x894e24),
-    CENTRAL (2, "Central", 0x894e24),
-    CIRCLE (3, "Circle", 0x894e24),
-    DISTRICT (4, "District", 0x894e24),
-    DLR (5, "DLR", 0x894e24),
-    HAMMERSMITH_AND_CITY (6, "Hammersmith & City", 0x894e24),
-    JUBILEE (7, "Jubilee", 0x894e24),
-    LONDON_OVERGROUND (8, "London Overground", 0x894e24),
-    METROPOLITAN (9, "Metropolitan", 0x894e24),
-    NORTHERN (10, "Northern", 0x894e24),
-    PICADILLY (11, "Picadilly", 0x894e24),
-    VICTORIA (12, "Victoria", 0x894e24),
-    WATERLOO_AND_CITY (13, "Waterloo & City", 0x894e24);
+    BAKERLOO (1, "Bakerloo", "#894e24"),
+    CENTRAL (2, "Central", "#dc241f"),
+    CIRCLE (3, "Circle", "#ffce00"),
+    DISTRICT (4, "District", "#007229"),
+    DLR (5, "DLR", "#00afad"),
+    HAMMERSMITH_AND_CITY (6, "Hammersmith and City", "#d799af"),
+    JUBILEE (7, "Jubilee", "#6a7278"),
+    LONDON_OVERGROUND (8, "Overground", "#e86a10"),
+    METROPOLITAN (9, "Metropolitan", "#751056"),
+    NORTHERN (10, "Northern", "#000000"),
+    PICADILLY (11, "Piccadilly", "#0019a8"),
+    VICTORIA (12, "Victoria", "#00a0e2"),
+    WATERLOO_AND_CITY (13, "Waterloo and City", "#76d0bd");
 
     private final int value;
     private final String lineName;
     private final int colourCode;
 
-    TflLineColours(int value, String lineName, int colourCode) {
+    TflLineColours(int value, String lineName, String colourCodeAsHex) {
 
         this.value = value;
         this.lineName = lineName;
-        this.colourCode = colourCode;
-    }
-
-    public int getValue() {
-        return value;
+        this.colourCode = Color.parseColor(colourCodeAsHex);
     }
 
     public String getLineName() {
         return lineName;
     }
 
-    public int getColourCode() {
-        return colourCode;
+
+    public static int getColourForLineName(String name) {
+        for (TflLineColours tflLineColours : values()) {
+            if (tflLineColours.getLineName().equalsIgnoreCase(name)) {
+                return tflLineColours.colourCode;
+            }
+        }
+        return Color.BLACK;
     }
 }
